09f12f52ce88846e7ac29a03ed6d9b284e6b9d6d
iron maiden <ruancarlosdejesus8@gmail.com>
2025-08-01 20:02:46 -0300

    logo svg adicionada/logo adicionada no login/ login com google adicionado / atualização nas questoes/ niveis adicionados/ mudança na opção de escolha/ metadado modificado

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import  Cat from"../../public/Cat.png"
+
 import Catt from"../../public/Cat2.jpg";
 import { useNavigate, Link } from "react-router-dom";
 import { auth , provider, signInWithPopup } from '../services/firebaseConnections';
